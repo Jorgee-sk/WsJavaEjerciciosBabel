b84423e40e1e946729cc8bf1726052578c5b0cc3
Jorgee_sk <jgaliano2000@gmail.com>
2022-03-10 21:20:30 +0100

    cambios para comprobaciones del login

--- 02_EjercicioLoginWeb/src/main/java/servlets/LoginServlet.java
+++ 02_EjercicioLoginWeb/src/main/java/servlets/LoginServlet.java
@@ -27,37 +27,37 @@ public class LoginServlet extends HttpServlet {
 		user.setUsername(username);
 		user.setPassword(password);
 
-		Usuario user1 = new Usuario("Usuario","superSecreto");
-		Usuario user2 = new Usuario("Usuario2","1234");
-		Usuario user3 = new Usuario("xX_Usuario3_Xx","fechademicumple");
-		
+		Usuario user1 = new Usuario("Usuario", "superSecreto");
+		Usuario user2 = new Usuario("Usuario2", "1234");
+		Usuario user3 = new Usuario("xX_Usuario3_Xx", "fechademicumple");
+
 		ArrayList<Usuario> listaUsuarios = new ArrayList<Usuario>();
 		listaUsuarios.add(user1);
 		listaUsuarios.add(user2);
 		listaUsuarios.add(user3);
-		
+
 		System.out.println(user.getUsername());
 		
-		for(int i=0;i<3;i++) {
-			if (user.getUsername().equals(listaUsuarios.get(i).getUsername()) &&
-				user.getPassword().equals(listaUsuarios.get(i).getPassword())) 
-				{
-					RequestDispatcher rd = request.getRequestDispatcher("JspLoginSucces.jsp");
-					request.setAttribute("user", user);
-					request.setAttribute("mSucces", mSucces);
-					request.setAttribute("mNSucces", mNotSucces);
-					rd.forward(request, response);
-				}else{
-					if(i==3) {
-						RequestDispatcher rd = request.getRequestDispatcher("JspLoginNotSucces.jsp");
-						request.setAttribute("user", user);
-						request.setAttribute("mSucces", mSucces);
-						request.setAttribute("mNSucces", mNotSucces);
-						rd.forward(request, response);
-					}
-				}
+		
+		//Comprobamos primero si coincide con alguno de los 3 nombres
+		for (int i = 0; i < 3; i++) {
+			if (user.getUsername().equals(listaUsuarios.get(i).getUsername())
+					&& user.getPassword().equals(listaUsuarios.get(i).getPassword())) {
+				RequestDispatcher rd = request.getRequestDispatcher("JspLoginSucces.jsp");
+				request.setAttribute("user", user);
+				request.setAttribute("mSucces", mSucces);
+				request.setAttribute("mNSucces", mNotSucces);
+				rd.forward(request, response);
+			}
 		}
-			
+		//En cualquier otro caso vamos a saber que no ha sido successfull!
+		RequestDispatcher rd = request.getRequestDispatcher("JspLoginNotSucces.jsp");
+		request.setAttribute("user", user);
+		request.setAttribute("mSucces", mSucces);
+		request.setAttribute("mNSucces", mNotSucces);
+		rd.forward(request, response);
+		
+		
 
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
